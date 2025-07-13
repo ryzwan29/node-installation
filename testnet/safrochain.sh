bb627dc18888ff7c61157101c07fcf478dd83d10
Rizwan Fairuz Mamduh <rizwanfairuzmamduh29@gmail.com>
2025-07-13 21:56:48 +0700

    Update safrochain.sh

--- testnet/safrochain.sh
+++ testnet/safrochain.sh
@@ -100,7 +100,7 @@ sed -i '/\[rpc\]/,/^\[/{s/^enable *=.*/enable = true/}' $HOME/.safrochain/config
 sed -i '/\[rpc\]/,/^\[/{s|^laddr *=.*|laddr = "tcp://0.0.0.0:'"${SAFRO_PORT}"'657"|}' $HOME/.safrochain/config/config.toml
 
 # set custom ports in app.toml
-sed -i.bak -e "s%:1317%:${SAFRO_PORT}317%g;
+sed -i.bak -e "s%:1317%:${SAFRO_PORT}17%g;
 s%:8080%:${SAFRO_PORT}080%g;
 s%:9090%:${SAFRO_PORT}090%g;
 s%:9091%:${SAFRO_PORT}091%g;
@@ -153,4 +153,122 @@ mv $HOME/.safrochain/priv_validator_state.json.backup $HOME/.safrochain/data/pri
 sudo systemctl restart safrochaind
 
 # Create Endpoint Domain
+read -p "Input Your Domain: (e.g provewithryd.xyz)" DOMAIN
+SSL_PATH="/etc/letsencrypt/live/$DOMAIN"
+DOMAIN_RPC="testnet-rpc-safrochain.$DOMAIN"
+DOMAIN_GRPC="testnet-grpc-safrochain.$DOMAIN"
+DOMAIN_API="testnet-rpc-safrochain.$DOMAIN"
 
+# Create Endpoint Domain
+# === [RPC] ===
+sudo tee /etc/nginx/sites-available/$DOMAIN_RPC.conf > /dev/null <<EOF
+server {
+    listen 80;
+    server_name $DOMAIN_RPC;
+    return 301 https://\$host\$request_uri;
+}
+server {
+    listen 443 ssl;
+    server_name $DOMAIN_RPC;
+
+    ssl_certificate $SSL_PATH/fullchain.pem;
+    ssl_certificate_key $SSL_PATH/privkey.pem;
+
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers HIGH:!aNULL:!MD5;
+
+    add_header 'Access-Control-Allow-Methods' 'GET,POST,OPTIONS';
+    add_header 'Access-Control-Allow-Headers' 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range';
+
+    location / {
+        proxy_pass http://0.0.0.0:${SAFRO_PORT}657;
+
+        add_header Access-Control-Allow-Origin *;
+        add_header Access-Control-Max-Age 3600;
+        add_header Access-Control-Expose-Headers Content-Length;
+
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+
+# === gRPC Endpoint ===
+sudo tee /etc/nginx/sites-available/$DOMAIN_GRPC.conf > /dev/null <<EOF
+server {
+    listen 80;
+    server_name $DOMAIN_GRPC;
+    return 301 https://\$host\$request_uri;
+}
+server {
+    listen 443 ssl;
+    server_name $DOMAIN_GRPC;
+
+    ssl_certificate $SSL_PATH/fullchain.pem;
+    ssl_certificate_key $SSL_PATH/privkey.pem;
+
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers HIGH:!aNULL:!MD5;
+
+    location / {
+        if (\$request_method = OPTIONS) {
+            add_header 'Access-Control-Allow-Origin' '*';
+            add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+            add_header 'Access-Control-Allow-Headers' 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range';
+            add_header 'Access-Control-Max-Age' 1728000;
+            add_header 'Content-Length' 0;
+            add_header 'Content-Type' 'text/plain charset=UTF-8';
+            return 204;
+        }
+
+        proxy_pass http://0.0.0.0:${SAFRO_PORT}90;
+
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Max-Age' 3600;
+        add_header 'Access-Control-Expose-Headers' 'Content-Length';
+
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+
+# === API Endpoint ===
+sudo tee /etc/nginx/sites-available/$DOMAIN_API.conf > /dev/null <<EOF
+server {
+    listen 443 ssl http2;
+    server_name $DOMAIN_API;
+
+    ssl_certificate $SSL_PATH/fullchain.pem;
+    ssl_certificate_key $SSL_PATH/privkey.pem;
+
+    location / {
+        proxy_pass http://0.0.0.0:${SAFRO_PORT}17;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+
+        proxy_hide_header Access-Control-Allow-Origin;
+
+        add_header 'Access-Control-Allow-Origin' '*' always;
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
+        add_header 'Access-Control-Allow-Headers' 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Length' always;
+
+        if (\$request_method = OPTIONS) {
+            add_header 'Content-Length' 0;
+            add_header 'Content-Type' 'text/plain; charset=UTF-8';
+            return 204;
+        }
+    }
+}
+EOF
+
+# Enable all
+sudo ln -sf /etc/nginx/sites-available/$DOMAIN_RPC.conf /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/$DOMAIN_GRPC.conf /etc/nginx/sites-enabled/
+sudo ln -sf /etc/nginx/sites-available/$DOMAIN_API.conf /etc/nginx/sites-enabled/
+
+# Reload NGINX
+sudo nginx -t && sudo systemctl reload nginx
